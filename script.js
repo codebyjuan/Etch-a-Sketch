4f07fd27343fc612ab3265a0ad927af2fc443f18
Juan <jbellevue83022@gmail.com>
2024-10-14 22:02:07 -0500

    Added randomized color gen for etch-a-sketch

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const tileHeight = 700 / tilesPerRow;
 
 container.innerHTML = '';
 
+//work on the random color gen
+function randomColors() {
+    let hex = '#' + Math.floor(Math.random() * 16777215).toString(16); 
+    return hex;
+}
 
 const totalTiles = tilesPerRow * tilesPerRow;
 for(let i=0; i < totalTiles; i++) {
@@ -19,11 +24,12 @@ container.append(tile);
 tile.setAttribute('class', 'tile');
 tile.style.width = `${tileWidth}px`;
 tile.style.height = `${tileHeight}px`;
-}
+tile.addEventListener('mouseenter', () => tile.style.backgroundColor = randomColors());}
 
 }
+
+createGrid(16);
 
 sizeBtn.addEventListener('click', (e) => {let grid = prompt("What would you like the dimensions to be?"); createGrid(grid);});
-    
     
 
